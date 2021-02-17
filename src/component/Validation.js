27c4879ecb93d43a29ad3c30acfe36c8426b10bd
German G Sayago <german.sayago@gmail.com>
2021-02-17 18:25:43 -0300

    se agregaron logos en validation

--- src/component/Validation.js
+++ src/component/Validation.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import LogoBayer from '../assets/static/logo-bayer.png';
+import LogoDekalb from '../assets/static/logo-dekalb.png';
+import LogoRoundup from '../assets/static/logo-roundup.png';
 
 export default function Validation() {
   return (
@@ -7,12 +10,16 @@ export default function Validation() {
         <h3>
           <span>Marcas que trabajamos</span>
         </h3>
-        <div className='grid-container'>
-          <div className='grid-item'>logo</div>
-          <div className='grid-item'>logo</div>
-          <div className='grid-item'>logo</div>
-          <div className='grid-item'>logo</div>
-          <div className='grid-item'>logo</div>
+        <div className='row justify-content-center'>
+          <div className='col-6 col-md-3'>
+            <img src={LogoBayer} alt='bayer' className='img-fluid' />
+          </div>
+          <div className='col-6 col-md-3'>
+            <img src={LogoDekalb} alt='bayer' className='img-fluid' />
+          </div>
+          <div className='col-6 col-md-3'>
+            <img src={LogoRoundup} alt='bayer' className='img-fluid' />
+          </div>
         </div>
       </div>
     </section>
