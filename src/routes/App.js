bf3f4248b0ecd350d10474768dee06624354829b
German G Sayago <german.sayago@gmail.com>
2021-06-26 12:33:22 -0300

    fix install gtm

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,7 +5,7 @@ import NotFound from 'pages/NotFound';
 import Tanks from 'pages/Tanks';
 import Policy from 'pages/Policy';
 
-// import TagManager from 'react-gtm-module';
+import TagManager from 'react-gtm-module';
 import AboutUs from 'pages/AboutUs';
 import Products from 'pages/Products';
 import ScrollToTop from 'components/ScrollToTop';
